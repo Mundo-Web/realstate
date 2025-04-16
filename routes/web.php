0f771d62e6203608203df98897f9bea13addd43c
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-04-16 16:01:59 -0500

    final

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ use App\Models\NosotrosView;
 /* Las rutas publicas */
 // Route::get('/login-rev', [AuthController::class, 'loginView'])->name('Login.jsx');
 Route::get('/register-rev', [AuthController::class, 'registerView'])->name('Register.jsx');
-Route::get('/', [IndexController::class, 'index'])->name('index');
+Route::get('/inicio', [IndexController::class, 'index'])->name('index');
 Route::get('/nosotros', [IndexController::class, 'nosotros'])->name('nosotros');
 Route::get('/servicios', [IndexController::class, 'servicios'])->name('servicios');
 Route::get('/comentario', [IndexController::class, 'comentario'])->name('comentario');
