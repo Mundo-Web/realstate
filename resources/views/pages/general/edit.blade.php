f2b8c742c22d8bf5efe7f403dc20a722d64060ac
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-05-27 12:21:11 -0500

    final

--- resources/views/pages/general/edit.blade.php
+++ resources/views/pages/general/edit.blade.php
@@ -261,7 +261,7 @@
                                 </div>
                               </div>
 
-                              {{-- <div>
+                              <div>
                                 <label for="youtube">Youtube</label>
 
                                 <div class="relative ">
@@ -279,7 +279,7 @@
                                     class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Youtube">
                                 </div>
-                              </div> --}}
+                              </div>
 
                               {{-- <div>
                                 <label for="twitter">Twitter</label>
@@ -320,6 +320,7 @@
                                     placeholder="Linkedin">
                                 </div>
                               </div>
+
                               <div>
                                 <label for="tiktok">Tiktok</label>
 
